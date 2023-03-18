6c9dc18599094df490d0db229854ce9152e9ca66
₳₳₳₳₳₳₳ <dwomeco2@github.fake>
2023-03-18 09:09:13 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,82 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useRef } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter, faLinkedin, faFacebook, faInstagram, faPinterest, faFacebookMessenger } from '@fortawesome/free-brands-svg-icons';
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+
+interface SocialIconProps {
+  icon: IconDefinition;
+  link: string;
+}
+
+function SocialIconsList(): JSX.Element {
+  const [socialIcons, _setSocialIcons] = useState<SocialIconProps[]>([
+    {
+      icon: faTwitter,
+      link: 'https://twitter.com/',
+    },
+    {
+      icon: faLinkedin,
+      link: 'https://twitter.com/',
+    },
+    {
+      icon: faFacebook,
+      link: 'https://www.instagram.com/',
+    },
+    {
+      icon: faInstagram,
+      link: 'https://www.youtube.com/',
+    },
+    {
+      icon: faPinterest,
+      link: 'https://www.linkedin.com/',
+    },
+    {
+      icon: faFacebookMessenger,
+      link: 'https://github.com/',
+    }
+  ])
+  return (
+    <div className="flex justify-between my-4 gap-4">
+      {socialIcons.map(({icon}, index) => (
+        <button key={index} className="w-8 h-8 text-white bg-gray-300 rounded-full hover:ring-1 ring-black">
+          <FontAwesomeIcon icon={icon} />
+        </button>
+      ))}
+    </div>
+  )
+}
+
+function CopyInput({ link, inputRef }: { link: string, inputRef: React.RefObject<HTMLInputElement> }): JSX.Element {
+  return (
+    <div className="flex bg-gray-300 px-2 py-1 rounded-md text-xs">
+      <input ref={inputRef} type="text" value={link} className="grow bg-transparent outline-none" readOnly />
+      <button className="font-medium pl-2" onClick={() => navigator.clipboard.writeText(inputRef.current?.value ?? "")}>Copy</button>
+    </div>
+  )
+}
+
+function ShareCard() {
+  const [copyLink, _setCopyLink] = useState("https://www.copymehaha.com/aslkdowce");
+  const inputRef = useRef<HTMLInputElement>()  as React.MutableRefObject<HTMLInputElement>;
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="h-full px-10 py-6 bg-gray-100 rounded-xl">
+      <div className="font-bold text-sm">Share this challenge</div>
+      <SocialIconsList />
+      <div className="text-xs mb-2">or copy link</div>
+      <CopyInput link={copyLink} inputRef={inputRef} />
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <div className="flex h-screen items-center">
+      <div className="mx-auto">
+        <ShareCard />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
